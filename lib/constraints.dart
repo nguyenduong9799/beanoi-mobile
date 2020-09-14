fc2bfa5a391e738edd52bf5c679e5367935a87ac
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-09-14 11:14:26 +0700

    Add grey color pallet constraint

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -3,4 +3,14 @@ import 'package:flutter/material.dart';
 final kPrimary = Color(0xFF619a46);
 final kSecondary = Color(0xFF438029);
 
+final kBackgroundGrey = [
+  Color(0xFFFFFFFF),
+  Color(0xfffafafa),
+  Color(0xfff5f5f5),
+  Color(0xffe0e0e0),
+  Color(0xffbdbdbd),
+  Color(0xff9e9e9e),
+  Color(0xff757575),
+];
+
 final kTextPrimary = TextStyle(color: Color(0xFFFFFFFF));
