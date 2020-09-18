3b5e4d49e5db929774482c87b7377a33a3d5867d
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-09-18 13:53:08 +0700

    Add cart util for share_pref

--- lib/utils/shared_pref.dart
+++ lib/utils/shared_pref.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unidelivery_mobile/Model/DTO/CartDTO.dart';
 
 Future<bool> setFCMToken(String value) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -18,6 +21,20 @@ Future<bool> setToken(String value) async {
 Future<String> getToken() async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   return prefs.getString('token');
+}
+
+Future<Cart> setCart(Cart cart) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('CART', jsonEncode(cart));
+  return cart;
+}
+
+Future<Cart> getCart() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  String encodedCart = prefs.getString('CART');
+
+  Cart cart = jsonDecode(encodedCart);
+  return cart;
 }
 // Future<bool> setUser(A value) async {
 //   final SharedPreferences prefs = await SharedPreferences.getInstance();
