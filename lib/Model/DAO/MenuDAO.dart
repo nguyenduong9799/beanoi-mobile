e6056781c949d6ee0afa6868cf87d935c9dfb658
Smjle <denwingsa01884@gmail.com>
2022-09-05 10:35:32 +0700

    fix/ Page Size

--- lib/Model/DAO/MenuDAO.dart
+++ lib/Model/DAO/MenuDAO.dart
@@ -7,8 +7,9 @@ import 'package:unidelivery_mobile/Utils/index.dart';
 import 'index.dart';
 
 class MenuDAO extends BaseDAO {
-  Future<List<MenuDTO>> getMenus({int areaID}) async {
-    final res = await request.get('areas/$areaID/menus?type=1');
+  Future<List<MenuDTO>> getMenus({int areaID, int page, int size}) async {
+    final res = await request.get('areas/$areaID/menus?type=1',
+        queryParameters: {"size": size ?? DEFAULT_SIZE, "page": page ?? 1});
     var jsonList = res.data["data"] as List;
     //metaDataDTO = MetaDataDTO.fromJson(res.data["metadata"]);
     if (jsonList != null) {
