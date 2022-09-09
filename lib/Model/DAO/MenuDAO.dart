b8c25d6d37c0f5a62a4ed927c164510a20ee3803
Endy <nguyenduong971999@gmail.com>
2022-09-09 17:38:45 +0700

    fix: fix bug orders

--- lib/Model/DAO/MenuDAO.dart
+++ lib/Model/DAO/MenuDAO.dart
@@ -7,8 +7,18 @@ import 'package:unidelivery_mobile/Utils/index.dart';
 import 'index.dart';
 
 class MenuDAO extends BaseDAO {
-  Future<List<MenuDTO>> getMenus({int areaID}) async {
-    final res = await request.get('areas/$areaID/menus?type=1');
+  Future<List<MenuDTO>> getMenus(
+    int areaID, {
+    int page,
+    int size,
+    int type,
+    Map<String, dynamic> params = const {},
+  }) async {
+    final res = await request.get(
+      'areas/$areaID/menus?type=1',
+      queryParameters: {"page": page ?? 1, "size": size ?? DEFAULT_SIZE}
+        ..addAll(params),
+    );
     var jsonList = res.data["data"] as List;
     //metaDataDTO = MetaDataDTO.fromJson(res.data["metadata"]);
     if (jsonList != null) {
